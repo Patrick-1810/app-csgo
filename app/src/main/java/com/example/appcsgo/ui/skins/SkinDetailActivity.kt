61e17be251ca892f22608f6cdef650c09c0dfc6a
Patrick-1810 <patrickprestes977@gmail.com>
2025-11-16 23:50:23 -0300

    feat: Implement Skin Detail UI and Up Navigation

--- app/src/main/java/com/example/appcsgo/ui/skins/SkinDetailActivity.kt
+++ app/src/main/java/com/example/appcsgo/ui/skins/SkinDetailActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import coil.load
 import com.example.appcsgo.R
 import com.example.appcsgo.databinding.ActivitySkinDetailBinding
@@ -18,6 +19,19 @@ class SkinDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySkinDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val toolbar: Toolbar = binding.toolbarSkinDetail
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+            title = ""
+        }
+
+        toolbar.setNavigationOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
         
         val json = intent.getStringExtra(EXTRA_JSON)
         val skin = Gson().fromJson(json, Skin::class.java)
